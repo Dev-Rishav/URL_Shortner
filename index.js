0e7a76d4c861284459def7afa2ffeefb6db6df04
Dev-Rishav <rishavchakraborty94@gmail.com>
2024-09-04 18:47:24 +0530

    cutom middleware to keep track record for total visits

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 const express=require ("express");
 const {connectMongoDB}=require("./MongoDBConnection");
+const fs=require("fs");
 
 const app=express();
 
 //connection
 connectMongoDB('mongodb://localhost:27017/url_shortner').then(()=>console.log("MongoDB connected"));
 
+//* custom middleware (This is where log will be created for the clicks/visits in the page) 
+app.use((req,res,next)=>{
+    fs.appendFile('log.txt',`\n${Date.now()} ${req.ip} ${req.method}: ${req.path}`,(err,data)=>{
+        next();
+    })
+})
 
 //routes
 app.get('/',(req,res)=>{
